51fb2bed9d057ecc27b74d16b6d1f01bd996c16a
mattblittle-create <matt.b.little@gmail.com>
2025-08-13 14:49:06 -0400

    Update SiteHeader.tsx

--- app/components/SiteHeader.tsx
+++ app/components/SiteHeader.tsx
@@ -115,8 +115,8 @@ export default function SiteHeader() {
             <Image
               src="/scoutline-logo-gold.png"
               alt="ScoutLine"
-              width={360}
-              height={80}
+              width={250}
+              height={100}
               priority
               style={{ width: "100%", height: "auto", display: "block" }}
             />
