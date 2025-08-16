817c1bfb577161bac332076e4b3795ffb0b418f9
mattblittle-create <matt.b.little@gmail.com>
2025-08-16 15:00:56 -0400

    Update page.tsx

--- app/recruiting-journey/page.tsx
+++ app/recruiting-journey/page.tsx
@@ -26,8 +26,8 @@ export default function RecruitingJourneyPage() {
           <li>Quickly and easily update metrics, stats, and player info</li>
           <li>Coach, parent and team admin views that stay in sync with player updates</li>
           <li>Private communication between player and coaches</li>
-          <li>Track progress over time across key performance indicators</li>
           <li>Get feedback on the effectiveness of your recruiting profile</li>
+          <li>Track progress over time across key performance indicators</li>
           <li>Ability to connect YouTube and social media links for higlight videos</li>
         </ul>
       </main>
