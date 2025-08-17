79f8299f2167985a4ca91b37e18d1c755843b036
mattblittle-create <matt.b.little@gmail.com>
2025-08-17 18:42:20 -0400

    Update page.tsx

--- app/recruiting-journey/page.tsx
+++ app/recruiting-journey/page.tsx
@@ -1,12 +1,10 @@
 // app/recruiting-journey/page.tsx
 import RecruitingJourneyHero from "../components/RecruitingJourneyHero";
-import RecruitingJourneySearch from "../components/RecruitingJourneySearch";
 
 export default function RecruitingJourneyPage() {
   return (
     <>
       <RecruitingJourneyHero />
-      <RecruitingJourneySearch />
     </>
   );
 }
