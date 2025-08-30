0890ec62f399ef86a92ddb13e1ee8b6cb59af58a
Matt Little <matt.b.little@gmail.com>
2025-08-30 09:05:50 -0400

    db: safe migration to add User.program; remove failed migration

--- prisma/migrations/20250829162746_add_user_program/migration.sql
+++
@@ -1,2 +0,0 @@
-﻿-- Add missing program column to User
-ALTER TABLE "User" ADD COLUMN IF NOT EXISTS "program" TEXT;
---
+++ prisma/migrations/20250830090540_add_user_program_safe/migration.sql
@@ -0,0 +1,2 @@
+﻿-- Safe, idempotent migration: add column if it isn't there yet
+ALTER TABLE "User" ADD COLUMN IF NOT EXISTS "program" TEXT;
