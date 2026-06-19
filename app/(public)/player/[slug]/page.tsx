93e57858be20c0c67becc7135bc46e7771f94c23
Matt Little <matt.b.little@gmail.com>
2026-06-19 13:20:22 -0400

    u

--- app/(public)/player/[slug]/page.tsx
+++ app/(public)/player/[slug]/page.tsx
@@ -156,7 +156,7 @@ function JumpToSectionNav({ sections }: { sections: { id: string; label: string 
 }
 
 const SECTION_SCROLL_MARGIN_DESKTOP = 300;
-const SECTION_SCROLL_MARGIN_MOBILE = 200;
+const SECTION_SCROLL_MARGIN_MOBILE = 250;
 
 function SectionWrapper({
   id,
